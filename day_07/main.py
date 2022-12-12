374e69aefd4823989e40e94121028571ec89538f
Nick Swainston <nickaswainston@gmail.com>
2022-12-12 16:35:29 +0800

    Fixes for keegan's day_07

--- day_07/main.py
+++ day_07/main.py
@@ -2,7 +2,7 @@ import os
 
 current_dir = ""
 dir_sizes = {"/": 0}
-with open('input.txt') as data_file:
+with open('keegans_input.txt') as data_file:
     for line in data_file:
         line = line[:-1]
         if line.startswith("$ cd"):
@@ -27,10 +27,10 @@ for depth in range(max(most)-1, 0, -1):
     print(depth)
     for dir_key in dir_sizes.keys():
         if dir_key.count("/") == depth and dir_key != "/":
-            print(dir_key)
+            print(f"\n{dir_key}")
             # Write depth so find all that are one deeper
             for deeper_key in dir_sizes.keys():
-                if deeper_key.count("/") == depth + 1 and dir_key in deeper_key:
+                if deeper_key.count("/") == depth + 1 and deeper_key.startswith(dir_key):
                     print(deeper_key)
                     # Update the size
                     dir_sizes[dir_key] += dir_sizes[deeper_key]
@@ -49,7 +49,7 @@ print(f"Part 1: {part1_sum}")
 
 size_needed = 30000000 - (70000000 - dir_sizes["/"])
 sorted_dir_sizes = sorted(dir_sizes.items(), key=lambda x:x[1])
-print(sorted_dir_sizes)
+#print(sorted_dir_sizes)
 print(size_needed)
 for dir_name, size in sorted_dir_sizes:
     if size > size_needed:
